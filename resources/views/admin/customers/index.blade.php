3baf42495f7c3d5975231188053ca2c43a794a68
lovranitstamas <tamas198601@gmail.com>
2021-03-13 21:20:05 +0100

    Modify admin customer list

--- resources/views/admin/customers/index.blade.php
+++ resources/views/admin/customers/index.blade.php
@@ -2,43 +2,55 @@
 
 @section('content')
 
-    <h4>Ügyfelek</h4>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Ügyfél lista</h3>
+                    <div class="card-tools">
+                        <a href="{{route('admin.customers.create')}}" class="nav-link">
+                            <p>Ügyfél létrehozása</p>
+                        </a>
+                    </div>
+                </div>
 
-    @if(session()->has('message'))
-        <h3>{{session('message')}}</h3>
-    @endif
+                <div class="card-body table-responsive p-0">
 
 
-    <form action="{{route('admin.customers.index')}}" method="GET">
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <td>{!! orderTableHeader('id', '#') !!}</td>
-                <td>{!! orderTableHeader('name', 'Név') !!}</td>
-                <td>{!! orderTableHeader('email', 'E-mail') !!}</td>
-                <td>Módosítás</td>
-            </tr>
-            <tr>
-                <td><input type="text" class="form-control" name="search[name]" value="{{request()->input('search.name')
+                    <form action="{{route('admin.customers.index')}}" method="GET">
+                        <table class="table table-hover text-wrap">
+                            <thead>
+                            <tr>
+                                <td>{!! orderTableHeader('id', '#') !!}</td>
+                                <td>{!! orderTableHeader('name', 'Név') !!}</td>
+                                <td>{!! orderTableHeader('email', 'E-mail') !!}</td>
+                                <td>Módosítás</td>
+                            </tr>
+                            <tr>
+                                <td><input type="text" class="form-control" name="search[name]" value="{{request()->input('search.name')
                 }}"></td>
-                <td colspan="2"></td>
-                <td><input type="submit" class="btn btn-primary" value="keresés"></td>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($customers as $customer)
-                <tr id="customer-{{$customer->id}}">
-                    <td>{{$customer->id}}</td>
-                    <td>{{$customer->name}}</td>
-                    <td>{{$customer->email}}</td>
-                    <td><a class="btn btn-default" href="{{route('admin.customers.edit', ['id' => $customer->id])
+                                <td colspan="2"></td>
+                                <td><input type="submit" class="btn btn-primary" value="keresés"></td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($customers as $customer)
+                                <tr id="customer-{{$customer->id}}">
+                                    <td>{{$customer->id}}</td>
+                                    <td>{{$customer->name}}</td>
+                                    <td>{{$customer->email}}</td>
+                                    <td><a class="btn btn-default" href="{{route('admin.customers.edit', ['id' => $customer->id])
                     }}">Módosítás <i
-                                class="fa fa-edit"></i></a></td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </form>
+                                                class="fa fa-edit"></i></a></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 @endsection {{-- @stop--}}
