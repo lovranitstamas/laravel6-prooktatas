0ff41715a2e946ddd3336dae44a4b535356facf6
lovranitstamas <tamas198601@gmail.com>
2021-03-21 19:01:38 +0100

    Add caret icon to table order (admin customer)

--- app/helpers.php
+++ app/helpers.php
@@ -6,15 +6,25 @@ if (!function_exists('orderTableHeader')) {
     {
 
         $currentRouteName = \Route::currentRouteName();
-        return '<a href="' . route($currentRouteName,
-            [
-                'search' => request()->input('search'),
-                'orderBy' => $field,
-                'orderDir' =>
-                    request()->input('orderBy') == $field &&
-                    request()->input('orderDir') == 'asc' ? 'desc' : 'asc'
-            ]) . '">' . $tableHeaderText . '</a>';
+        $link = '<a href="' . route($currentRouteName,
+                [
+                    'search' => request()->input('search'),
+                    'orderBy' => $field,
+                    'orderDir' =>
+                        request()->input('orderBy') == $field &&
+                        request()->input('orderDir') == 'asc' ? 'desc' : 'asc'
+                ]) . '">' . $tableHeaderText . '</a>';
 
+        $link .= ' <i class="fa fa-caret-';
+        if (request()->input('orderBy') == $field &&
+            request()->input('orderDir') == 'asc') {
+            $link .= 'up';
+        } else {
+            $link .= 'down';
+        }
+        $link .= '"></i>';
+
+        return $link;
     }
 }
 
