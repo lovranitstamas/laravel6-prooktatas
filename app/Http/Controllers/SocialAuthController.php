122d43ccc2824588953d0dba6b6f71e19c57d6fa
lovranitstamas <tamas198601@gmail.com>
2021-03-27 20:49:51 +0100

    Fb login

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Customer;
 use Socialite;
 
-class SocialAuthController extends Controller {
+class SocialAuthController extends Controller
+{
 
     /**
      * Redirect the user to the GitHub authentication page.
@@ -24,9 +27,52 @@ class SocialAuthController extends Controller {
      */
     public function handleProviderCallback($provider)
     {
-        $user = Socialite::driver($provider)->user();
 
-        dd($user);
+        $socialiteUserObject = Socialite::driver($provider)->stateless()->user();
+        //$socialiteUserObject = Socialite::driver($provider)->user();
+
+        //keresés JSON mezőben
+        //HA TALÁLUNK A FACEBOOK ID ALAPJÁN CUSTOMER
+        $customer = Customer::where([
+            'social->' . $provider . '->id' => $socialiteUserObject->getId(),
+        ])->first();
+
+
+        //Mivel a Modelben array-jé castoltuk a social atrributútumot, ezért ő tömbként visekedik.
+        // dd($customer->social['facebook']['id']);
+        if (!$customer) {
+            //HA FACEBOOK ID ALAPJÁN NINCS, AKKOR EMAIL ALAPJÁN
+            $customer = Customer::where('email', 'LIKE', $socialiteUserObject->getEmail())->first();
+            if (!$customer) {
+                //HA EMAIL ALAPJÁN SINCS, AKKOR LÉTREHOZZUK
+                $customer = $this->createNewCustomer($socialiteUserObject);
+            }
+        }
+        //$customer->social->facebook->token = $socialiteUserObject->token;
+        $socialArray = [
+            $provider => [
+                'id' => $socialiteUserObject->getId(),
+                'token' => $socialiteUserObject->token
+            ]
+        ];
+
+        $customer->social = $socialArray;
+        $customer->save();
+
+        auth()->guard('customer')->loginUsingId($customer->id);
+
+        return redirect()->route('customers.index');
     }
+
+    protected function createNewCustomer($socialiteUserObject)
+    {
+        $customer = new Customer;
+        $customer->name = $socialiteUserObject->getName();
+        $customer->email = $socialiteUserObject->getEmail();
+        $customer->description = 'teszt';
+
+        return $customer;
+    }
+
 
 }
